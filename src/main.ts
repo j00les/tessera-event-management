d2713af5b54e6f721de17aed1b7456a3dfa7c1bf
Nabiel Alif <nabiel.alif01@gmail.com>
2024-06-16 21:40:48 +0700

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppDataSource } from 'data-source';
 import { AppModule } from './config/app.module';
@@ -8,8 +9,16 @@ async function bootstrap() {
   await AppDataSource.initialize();
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(validationPipe);
+  const config = new DocumentBuilder()
+    .setTitle('Tessera Event Management')
+    .setDescription('api for creating and fetching city and event data')
+    .setVersion('1.0')
+    .addTag('Event Management API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
+  app.useGlobalPipes(validationPipe);
   await app.listen(3000);
 }
 bootstrap();
